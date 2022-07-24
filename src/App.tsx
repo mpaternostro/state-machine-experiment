c50c331ed6b41daa17b69ff36a35ea7d63368a81
Marcelo <marcelodpaternostro@gmail.com>
2022-07-24 13:21:44 -0300

    feat: button disabled state while submitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import { classNames } from "./lib/classNames";
 
 export default function App() {
+	const [disqualifyState, setDisqualifyState] = useState<"idle" | "submitting">(
+		"idle",
+	);
 	// here everything is mixed up, form submission and business logic belongs to the same function
 	function handleDisqualify(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault();
 
+		setDisqualifyState("submitting");
+
 		// fake business logic
 		return new Promise((resolve) => {
 			setTimeout(() => {
+				setDisqualifyState("idle");
 				console.log("disqualified");
 				resolve("Success");
 			}, 2000);
 		});
 	}
 
-	function handleAdvanceToOffer(event: FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-
-		return new Promise((_resolve, reject) => {
-			setTimeout(() => {
-				reject("Could not advance to offer");
-			}, 2000);
-		});
-	}
+	const isDisqualifying = disqualifyState === "submitting";
 
 	return (
 		<main className="max-w-7xl mx-auto px-4 py-6 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:px-8">
@@ -59,17 +58,16 @@ export default function App() {
 				<form onSubmit={handleDisqualify}>
 					<button
 						type="submit"
-						className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+						className={classNames(
+							"w-32 inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500",
+							isDisqualifying ? "opacity-50 cursor-not-allowed" : "",
+						)}
+						aria-disabled={isDisqualifying}
 					>
-						Disqualify
-					</button>
-				</form>
-				<form onSubmit={handleAdvanceToOffer}>
-					<button
-						type="submit"
-						className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-					>
-						Advance to offer
+						{isDisqualifying ? "Disqualifying..." : "Disqualify"}
+						<span aria-live="assertive" className="sr-only">
+							{isDisqualifying ? "Disqualifying, please wait..." : ""}
+						</span>
 					</button>
 				</form>
 			</div>
