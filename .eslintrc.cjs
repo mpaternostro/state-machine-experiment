3531f30e883fff87e700b13cbe6b37d6b5db54e4
Marcelo <marcelodpaternostro@gmail.com>
2022-07-24 12:08:26 -0300

    feat: npm scripts and lint project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,45 +2,45 @@
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-  root: true,
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
-      },
-      plugins: ["@typescript-eslint"],
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      rules: {
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-misused-promises": [
-          "error",
-          {
-            checksVoidReturn: false,
-          },
-        ],
-      },
-    },
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
-  env: {
-    browser: true,
-    node: true,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+	root: true,
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				tsconfigRootDir: __dirname,
+				project: ["./tsconfig.json"],
+			},
+			plugins: ["@typescript-eslint"],
+			extends: [
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+			],
+			rules: {
+				"@typescript-eslint/no-non-null-assertion": "off",
+				"@typescript-eslint/no-misused-promises": [
+					"error",
+					{
+						checksVoidReturn: false,
+					},
+				],
+			},
+		},
+	],
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+		"prettier",
+	],
+	env: {
+		browser: true,
+		node: true,
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
